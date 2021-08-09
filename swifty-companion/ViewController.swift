7fbd9a1643785ca12199d8bc75241900c45746dc
Groosove <arturianec2013@gmail.com>
2021-08-09 16:01:20 +0300

    Start project

---
+++ swifty-companion/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  swifty-companion
+//
+//  Created by Fenix Lavon on 8/9/21.
+//  Copyright © 2021 Fenix Lavon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .red
+	}
+
+
+}
+
